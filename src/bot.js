672120fc6cad307130b4c10da6dc7b8d2ea1bda9
Michael Maysonet <michaelmaysonet74@gmail.com>
2021-02-09 20:06:52 +0000

    Add ping command

--- src/bot.js
+++ src/bot.js
@@ -1,1 +1,19 @@
-console.log("beep boop! 🤖");
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+require("dotenv").config();
+
+client.on("ready", () => {
+    console.log("beep boop! 🤖");
+});
+
+client.on("message", (msg) => {
+    const { author, content, reply } = msg;
+
+    if (content.match(/ping/gi)) {
+        console.log(author);
+        reply("pong...beep boop! 🤖");
+    }
+});
+
+client.login(process.env.DISCORD_TOKEN);
