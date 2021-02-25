c85a05202685cbb6788accc2e6dbceb4b3318561
Michael Maysonet <michaelmaysonet74@gmail.com>
2021-02-25 01:55:12 +0000

    wip

--- src/bot.js
+++ src/bot.js
@@ -8,22 +8,19 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 client.on("message", (msg) => {
-  if (
-	  !process.env.BOT_ID ||
-	  msg.author.id === process.env.BOT_ID
-  ) {
-	  return;
-	}
+    if (!process.env.BOT_ID || msg.author.id === process.env.BOT_ID) {
+        return;
+    }
 
-  try {
-    const { content } = msg;
-    const { command, flags } = extractCommandAndFlags(content);
-    const processedFlags = processFlags(flags);
-    CommandHandlers[command?.toLowerCase()](msg, processedFlags);
-  } catch (err) {
-    console.error(err);
-    msg.reply("Oops! Something went wrong 💔");
-  }
+    try {
+        const { content } = msg;
+        const { command, flags } = extractCommandAndFlags(content);
+        const processedFlags = processFlags(flags);
+        CommandHandlers[command?.toLowerCase()](msg, processedFlags);
+    } catch (err) {
+        console.error(err);
+        msg.reply("Oops! Something went wrong 💔");
+    }
 });
 
 client.login(process.env.DISCORD_TOKEN);
